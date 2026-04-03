6439475e5766bb5668dee469d9b7cf241f507fbe
Levi <garcialevi65@gmail.com>
2026-04-03 12:34:33 -0300

    feat: add login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+
 const App = () => {
   return (
   <>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}/>
+      </Routes>
+    </BrowserRouter>
   </>
   );
 }
